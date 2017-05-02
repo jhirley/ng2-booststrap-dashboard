389e59ab13b3b1fc9654c5b4343e3660d479bff4
Jhirley <jhirley.fonte@gmail.com>
2017-05-02 18:18:56 -0400

    added bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./bootstrap.min.css', './bootstrap-theme.min.css', './app.component.css']
 })
 export class AppComponent {
   title = 'app works!';
